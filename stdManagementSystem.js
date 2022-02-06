81be0103b32acc088ce66fe428e9038346da2311
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-02-06 17:34:40 +0530

    UC1 & UC2 added

---
+++ stdManagementSystem.js
@@ -0,0 +1,9 @@
+//UC 1&2: Adding and displaying student data in student management system.
+const studentDetails = [
+    {rollNo: 1, firstName:'Vivek', lastName: 'Singh', address:'UnityApt', physicsMarks: 95, chemistryMarks:87,mathMarks:92},
+    {rollNo: 2, firstName:'Rohit',lastName: 'Raj', address:'UtsavApt', physicsMarks:97, chemistryMarks:95,mathMarks:83},
+    {rollNo: 3, firstName:'Vikash',lastName: 'Malhotra',address:'AasthaKunj', physicsMarks:38, chemistryMarks:70,mathMarks:42},
+    ];
+
+
+console.log('Adding student data and displaying it ',studentDetails);
